efb8294d199885e0141f71a7ed964f059beba4ca
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-19 16:23:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignments_3._2._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Search(new int[] { 1, 5, 3 }, 5));
+            Console.WriteLine(Search(new int[] { 9, 8, 3 }, 3));
+            Console.WriteLine(Search(new int[] { 1, 2, 3 }, 4));
+        }
+
+        static int Search(int[] array, int target)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
+
